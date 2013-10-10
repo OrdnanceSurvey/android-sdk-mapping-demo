6a14bec09741bf41f67c4cb7b1e28a97b7c6f0bb
rob-murray <robmurray17@gmail.com>
2013-10-10 12:24:05 +0100

    refactor MainActivity; namespace, remove geocoder, make api keys static vars

--- OSMappingDemo/OSMappingDemo/src/main/java/uk/co/ordnancesurvey/android/demos/osmappingdemo/MainActivity.java
+++ OSMappingDemo/OSMappingDemo/src/main/java/uk/co/ordnancesurvey/android/demos/osmappingdemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ordnancesurvey.osmappingdemo;
+package uk.co.ordnancesurvey.android.demos.osmappingdemo;
 
 import android.os.Bundle;
 
@@ -14,51 +14,65 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 
-public class MainActivity extends FragmentActivity implements  OSMap.OnMapClickListener{
+public class MainActivity extends FragmentActivity implements OSMap.OnMapClickListener{
+
+    /**
+     *  Define your OS Openspace API KEY details below
+     *  @see http://www.ordnancesurvey.co.uk/oswebsite/web-services/os-openspace/index.html
+     *
+     */
+    private final static String OS_API_KEY = "DEMOAPI";
+
+    private final static boolean OS_IS_PRO = true;
+
+    private final static String OS_API_URL = "file:///";
+
+
     private final static String TAG = "MainActivity";
 
-    private Geocoder mGeocoder;
     private OSMap mMap;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         SupportMapFragment mf = (SupportMapFragment)fm.findFragmentById(R.id.map_fragment);
+
         mMap = mf.getMap();
 
+        //create list of tileSources
         ArrayList<OSTileSource> sources = new ArrayList<OSTileSource>();
 
-        // Web tile source.
-        sources.add(mMap.webTileSource("DEMOAPI", "file:///", true, null));
+        //create web tile source with API details
+        sources.add(mMap.webTileSource(OS_API_KEY, OS_API_URL, OS_IS_PRO, null));
         mMap.setTileSources(sources);
-        mMap.setOnMapClickListener(this);
-        mMap.setMyLocationEnabled(true);
 
-   try {
-            mGeocoder = new Geocoder(null, "DEMOAPI", "file:///", true);
-        }catch (Exception e) {
-                 e.printStackTrace();
-        }
+        // register as OnMapClickListener
+        mMap.setOnMapClickListener(this);
 
       }
 
 
     @Override
     protected void onDestroy() {
-        mGeocoder.close();
-        mGeocoder = null;
+
         super.onDestroy();
+
     }
 
     @Override
     public boolean onMapClick(GridPoint gp)
     {
-        Toast.makeText(this, "Tapped on map at " + gp.x + ", " + gp.y, Toast.LENGTH_SHORT).show();
+
+        final String locationMessage = String.format("Map tapped at OSGridPoint\n{%.0f, %.0f}", gp.x, gp.y);
+
+        Toast.makeText(this, locationMessage, Toast.LENGTH_SHORT).show();
         return true;
+
     }
 
 
